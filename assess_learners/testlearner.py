b06dbd27f223b2b01c075bf485e5eec47a448222
Pirashan <pirashan99@gmail.com>
2024-09-21 16:58:17 -0400

    DT Learner completed

--- assess_learners/testlearner.py
+++ assess_learners/testlearner.py
@@ -25,21 +25,30 @@ GT honor code violation.
   		  	   		 	   		  		  		    	 		 		   		 		  
 import math  		  	   		 	   		  		  		    	 		 		   		 		  
 import sys  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
-import numpy as np  		  	   		 	   		  		  		    	 		 		   		 		  
+import time
+import numpy as np
+import csv
   		  	   		 	   		  		  		    	 		 		   		 		  
 import LinRegLearner as lrl
 import DTLearner as dt
 import RTLearner as rt
+import BagLearner as bl
+import InsaneLearner as il
+
+import matplotlib.pyplot as plt
   		  	   		 	   		  		  		    	 		 		   		 		  
 if __name__ == "__main__":  		  	   		 	   		  		  		    	 		 		   		 		  
     if len(sys.argv) != 2:  		  	   		 	   		  		  		    	 		 		   		 		  
         print("Usage: python testlearner.py <filename>")  		  	   		 	   		  		  		    	 		 		   		 		  
-        sys.exit(1)  		  	   		 	   		  		  		    	 		 		   		 		  
-    inf = open(sys.argv[1])  		  	   		 	   		  		  		    	 		 		   		 		  
-    data = np.array(  		  	   		 	   		  		  		    	 		 		   		 		  
-        [list(map(float, s.strip().split(","))) for s in inf.readlines()]  		  	   		 	   		  		  		    	 		 		   		 		  
-    )  		  	   		 	   		  		  		    	 		 		   		 		  
+        sys.exit(1)
+    with open(sys.argv[1], 'r') as csvfile:
+        reader = csv.reader(csvfile)
+
+        # Skip the header row
+        next(reader)
+
+        # Convert the remaining data (skip date column) to float
+        data = np.array([[float(x) for x in row[1:]] for row in reader])
   		  	   		 	   		  		  		    	 		 		   		 		  
     # compute how much of the data is training and testing  		  	   		 	   		  		  		    	 		 		   		 		  
     train_rows = int(0.6 * data.shape[0])  		  	   		 	   		  		  		    	 		 		   		 		  
@@ -52,11 +61,37 @@ if __name__ == "__main__":
     test_y = data[train_rows:, -1]  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     print(f"{test_x.shape}")  		  	   		 	   		  		  		    	 		 		   		 		  
-    print(f"{test_y.shape}")  		  	   		 	   		  		  		    	 		 		   		 		  
-  		  	   		 	   		  		  		    	 		 		   		 		  
-    # create a learner and train it  		  	   		 	   		  		  		    	 		 		   		 		  
-    learner = lrl.LinRegLearner(verbose=True)  # create a LinRegLearner  		  	   		 	   		  		  		    	 		 		   		 		  
-    learner.add_evidence(train_x, train_y)  # train it  		  	   		 	   		  		  		    	 		 		   		 		  
+    print(f"{test_y.shape}")
+
+    # Question 1: Experiment 1 - DTLearner
+    rmse_in_sample = []
+    rmse_out_sample = []
+    for i in range(1, 51):
+        # create a learner and train it
+        learner = dt.DTLearner(leaf_size=i, verbose=True)
+        learner.add_evidence(train_x, train_y)
+        # evaluate in sample
+        pred_y = learner.query(train_x)
+        rmse = math.sqrt(((train_y - pred_y) ** 2).sum() / train_y.shape[0])
+        rmse_in_sample.append(rmse)
+        # evaluate out of sample
+        pred_y2 = learner.query(test_x)
+        rmse2 = math.sqrt(((test_y - pred_y2) ** 2).sum() / test_y.shape[0])
+        rmse_out_sample.append(rmse2)
+    # plotting the figure
+    plt.plot(rmse_in_sample)
+    plt.plot(rmse_out_sample)
+    plt.title("RMSE vs Leaf Size for DTLearner")
+    plt.xlabel("Leaf Size")
+    plt.xlim(1, 50)
+    plt.ylabel("RMSE")
+    plt.ylim(0, 0.01)
+    plt.legend(["In-Sample", "Out-Sample"])
+    plt.savefig("images/Experiment1.png")
+    plt.close("all")
+
+    learner = dt.DTLearner(leaf_size=1, verbose=False)  # constructor
+    learner.add_evidence(train_x, train_y)  # training step
     print(learner.author())  		  	   		 	   		  		  		    	 		 		   		 		  
   		  	   		 	   		  		  		    	 		 		   		 		  
     # evaluate in sample  		  	   		 	   		  		  		    	 		 		   		 		  
